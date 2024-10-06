91785a3cfa79b3287129c08653531ff632a2b5cd
abilisapiens <abili.sapiens@proton.me>
2024-10-06 12:59:21 -0400

    Add files via upload

--- beyondSunlight/js/scripts.js
+++ beyondSunlight/js/scripts.js
@@ -1,7 +1,9 @@
 function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
 }
